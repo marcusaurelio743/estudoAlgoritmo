1adb972efacc200ae36c851ebf9f59e4b827d102
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-03-14 17:53:55 -0300

    adicionando comentarios no codigo

--- Algoritimos/src/pesquisa/PesquisaBinariaLetra.java
+++ Algoritimos/src/pesquisa/PesquisaBinariaLetra.java
@@ -5,24 +5,26 @@ public class PesquisaBinariaLetra {
 	 public static Integer pesquisaBinaria(String[] lista, String item) {
 	        int baixo = 0;
 	        int alto = lista.length - 1;
-
+	        
+	        //criar um laço de repetição para percorrer toda estrutura
 	        while (baixo <= alto) {
-	            int meio = baixo + (alto - baixo) / 2;
-	            String chute = lista[meio];
+	        	
+	            int meio = baixo + (alto - baixo) / 2;//o meio da lista
+	            String chute = lista[meio];//pegar o valor da lista para um chute
 
-	            int comparacao = chute.compareTo(item);
+	            int comparacao = chute.compareTo(item);//comparar as duas strings
 
-	            if (comparacao == 0) {
+	            if (comparacao == 0) {// se for igual a zero significa que achou o item
 	            	System.out.println("igual");
-	                return meio;
+	                return meio; //retorna a posição do array
 	            }
 
 	            if (comparacao > 0) {
 	            	System.out.println("mais alto");
-	                alto = meio - 1;
+	                alto = meio - 1;// se for mais alto diminui uma posição
 	            } else {
 	            	System.out.println("mais baixo");
-	                baixo = meio + 1;
+	                baixo = meio + 1;// se for mais baixo aumenta uma posição
 	            }
 	        }
 
